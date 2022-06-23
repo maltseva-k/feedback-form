078c4b2198196ba246fb6ac2c198c7f8ebd7118a
maltseva-k <k.obydennova@gmail.com>
2022-06-23 11:25:54 +0300

    update js for ES6 and add comments

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-var submitted=false;
+// Запрещаем отправку до окончания проверки формы
+let submitted = false;
 
+// Получаем данные
 function getData() {
     return fetch('https://script.google.com/macros/s/AKfycbzGvKKUIaqsMuCj7-A2YRhR-f7GZjl4kSxSN1YyLkS01_CfiyE/exec?id=1EuEvFH4mM_GiE0CVpt3wJvk1lUhazj24D26kq5tFjss&sheet=Ответы на форму (2)')
         .then((response) => response.json())
 }
 
+// Забираем в Local Storage только массив штрихкодов
 getData()
     .then(data => data.records)
     .then(data => {
-        var items = data.map(function(item) {
+        let items = data.map(function(item) {
             return item.Barcode
         });
         return items
     })
     .then(data=> localStorage.setItem('searchResult', JSON.stringify(data)))
 
-
-
+// Проверяем валидность поля Имя
 function ValidName() {
-    var re = /^(\w|[а-яА-ЯёЁ ]+|-)+$/;
-    var myPhone = document.getElementById('name').value;
-    var valid = re.test(myPhone);
+    let re = /^(\w|[а-яА-ЯёЁ ]+|-)+$/;
+    let myPhone = document.getElementById('name').value;
+    let valid = re.test(myPhone);
     if (!valid) {
         document.getElementById('nameMessage').innerHTML = 'Введите корректные данные в поле Имя';
     } else {
@@ -29,19 +31,20 @@ function ValidName() {
     return valid;
 }
 
+// Проверяем валидность поля Email
 function ValidMail() {
-    var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
-    var myMail = document.getElementById('email').value;
-    var valid = re.test(myMail);
+    let re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+    let myMail = document.getElementById('email').value;
+    let valid = re.test(myMail);
     if (!valid) {
         document.getElementById('emailMessage').innerHTML = 'Введите корректные данные в поле Email';
     } else {
         document.getElementById('emailMessage').innerHTML = '';
     }
-
     return valid;
 }
 
+// Маска для ввода номера телфона только в заданном формате
 document.addEventListener('DOMContentLoaded', () => {
     IMask(
         document.getElementById('phone'), {
@@ -49,10 +52,12 @@ document.addEventListener('DOMContentLoaded', () => {
             lazy: true
         })
 })
+
+// Проверяем валидность поля Номер телефона
 function ValidPhone() {
-/*    var re = /^[\d\+][\d\(\)\ -]{4,14}\d$/;
-    var myPhone = document.getElementById('phone').value;
-    var valid = re.test(myPhone);*/
+/*  let re = /^[\d\+][\d\(\)\ -]{4,14}\d$/;
+    let myPhone = document.getElementById('phone').value;
+    let valid = re.test(myPhone); */
     let valid = false
     let phoneLength = document.getElementById('phone').value.length
     if (phoneLength !== 17) {
@@ -64,6 +69,8 @@ function ValidPhone() {
     }
     return valid
 }
+
+// Маска для ввода штрих-кода только в заданном формате
 document.addEventListener('DOMContentLoaded', () => {
     IMask(
         document.getElementById('barcode'), {
@@ -71,10 +78,12 @@ document.addEventListener('DOMContentLoaded', () => {
             lazy: true
         })
 })
+
+// Проверяем валидность поля Штрих-код
 function ValidBarcode() {
-    var re = /(^31|^32|^33|^34)(\d{11}$)/;
-    var myBarcode = document.getElementById('barcode').value;
-    var valid = re.test(myBarcode);
+    let re = /(^31|^32|^33|^34)(\d{11}$)/;
+    let myBarcode = document.getElementById('barcode').value;
+    let valid = re.test(myBarcode);
     if (!valid) {
         document.getElementById('barcodeMessage').innerHTML = 'Введите корректные данные в поле Штрих-код';
     } else {
@@ -83,13 +92,11 @@ function ValidBarcode() {
     return valid;
 }
 
+// Проверяем уникальность поля Штрих-код (т.е. исключаем повторную регистрацию штрих-кода)
 function CheckUnicBarcode() {
     let barcodeUnic = true
-    var barcodeValue = document.getElementById('barcode').value;
-
-
+    let barcodeValue = document.getElementById('barcode').value;
     const infoBarcodes = localStorage.getItem('searchResult') ? JSON.parse(localStorage.getItem('searchResult')) : []
-
     infoBarcodes.forEach((item)=>{
         if (item.toString() === barcodeValue.toString()) {
             barcodeUnic = false
@@ -97,22 +104,25 @@ function CheckUnicBarcode() {
     })
     if (barcodeUnic === true) {
         document.getElementById('barcodeMessage').innerHTML = '';
-
     } else {
         document.getElementById('barcodeMessage').innerHTML = 'Данный штрих-код уже зарегистрирован';
     }
-
     return barcodeUnic
-
-
 }
+
+// Проверяем введенные данные, если корректны - отправляем форму
 function checkData() {
+    ValidName()
+    ValidMail()
+    ValidPhone()
+    ValidBarcode()
+
     submitted = ValidName()&&ValidMail()&&ValidPhone()&&ValidBarcode()&&CheckUnicBarcode()
 
     return submitted
-
 }
 
+// Скрываем форму и выводим сообщение об успехе
 function hiddenFormBeforeSubmit() {
     if (submitted === true) {
         document.getElementById('formContent').style.display = 'none';
